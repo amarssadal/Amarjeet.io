bb3ea412e81afea34e3c3c0bed606055238f1627
Amarjeet Singh <amarssadal@gmail.com>
2020-05-30 19:16:00 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="main.css">
+        <link href="https://fonts.googleapis.com/css2?family=Caveat&family=Nothing+You+Could+Do&family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
+        <title>amarjeet.io - a space for his fooling arounds</title>
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="logoBox">
+                <div class="mainLogo">
+                    <img src="images/logo.png">
+                </div>
+            </div>
+            <div class="linkBox">
+                <div class="linkBoxContent">
+                    <ul class="link">    
+                        <span class="left bike">
+                            the oustanding
+                        </span>
+                        Bike Builder
+                        <span class="right bike">
+                            web app...kind of
+                        </span>
+                    </ul>
+                    <ul class="link">    
+                        <span class="left bike">
+                            precision
+                        </span>
+                        Contact Tracing
+                        <span class="right bike">
+                        </span>
+                    </ul>
+                    <ul class="link">    
+                        <span class="left bike">
+                            
+                        </span>
+                        Snake Game
+                        <span class="right bike">
+                            classic modern
+                        </span>
+                    </ul>
+                    <ul class="link">    
+                        <span class="left bike">
+                            sharp and on-point
+                        </span>
+                        Corona Timer
+                        <span class="right bike">
+                            to remind us all
+                        </span>
+                    </ul>
+                    <ul class="link">    
+                        <span class="left bike">
+                        </span>
+                        Calculator
+                        <span class="right bike">
+                            to calculate all
+                        </span>
+                    </ul>
+
+                    <ul class="link">    
+                        <span class="left bike">
+                        </span>
+                        To-Do App
+                        <span class="right bike">
+                            keeps you on track
+                        </span>
+                    </ul>
+                    <ul class="link">    
+                        <span class="left bike">
+                            canvas helps 
+                        </span>
+                        Magic Circles
+                        <span class="right bike">
+                            come to life
+                        </span>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ sub.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php $curr_page = $_GET['page']; ?>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="main.css">
+        <link href="https://fonts.googleapis.com/css2?family=Caveat&family=Nothing+You+Could+Do&family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
+        <title>amarjeet.io - a space for his fooling arounds</title>
+    </head>
+    <body>
+        <div class="subwrapper">
+            <div class="logoBox">
+                    <div class=logoBoxPositioner>
+                        <img class="logoImg" src="images/logo.png">
+                    </div>
+            </div>
+            <div class="linkBox">
+                <div class="link">
+                    <a href="sub.php?page=bike">Bike Builder</a>
+                </div>
+                <div class="link">
+                    Contact Tracing
+                </div>
+                <div class="link">
+                    Snake Game
+                </div>
+                <div class="link">
+                    Corona Timer
+                </div>
+                <div class="link">
+                    Calculator
+                </div>
+
+                <div class="link">
+                    To-Do App
+                </div>
+                <div class="link">
+                    Magic Circles
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
