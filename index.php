a825c1c8ce0898860a0a7d49168e6afbd49fa019
Amarjeet Singh <amarssadal@gmail.com>
2020-06-01 09:08:31 +0100

    more working changes addition of php

--- index.php
+++ index.php
@@ -1,84 +1,106 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="main.css">
-        <link href="https://fonts.googleapis.com/css2?family=Caveat&family=Nothing+You+Could+Do&family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
-        <title>amarjeet.io - a space for his fooling arounds</title>
-    </head>
-    <body>
+<?php
+    $techAvail = array('logo-javascript', 'logo-html5', 'logo-css3');
+    $resp = array('tablet-landscape-outline', 'arrow-forward-outline', 'phone-portrait-outline');
+
+    $projects = array(
+        'bike' => array(
+            'title' => 'Bike Builder',
+            'techList' => array_merge(array($techAvail[0], $techAvail[1], $techAvail[2]), $resp),
+            'gitLink' => 'gitlink'
+        ),
+        'contact' => array(
+            'title' => 'Contact Tracing',
+            'techList' => array_merge(array($techAvail[0], $techAvail[1], $techAvail[2]), $resp),
+            'gitLink' => 'gitlink'
+        ),
+        'snake' => array(
+            'title' => 'Snake Game',
+            'techList' => array_merge(array($techAvail[0], $techAvail[1], $techAvail[2]), $resp),
+            'gitLink' => 'gitlink'
+        ),
+        'corona' => array(
+            'title' => 'Corona Timer',
+            'techList' => array_merge(array($techAvail[0], $techAvail[1], $techAvail[2]), $resp),
+            'gitLink' => 'gitlink'
+        ),
+        'calc' => array(
+            'title' => 'Calculator',
+            'techList' => array_merge(array($techAvail[0], $techAvail[1], $techAvail[2]), $resp),
+            'gitLink' => 'gitlink'
+        ),
+        'todo' => array(
+            'title' => 'To-Do App',
+            'techList' => array_merge(array($techAvail[0], $techAvail[1], $techAvail[2]), $resp),
+            'gitLink' => 'gitlink'
+        ),
+        'canvas' => array(
+            'title' => 'Canvas Circles',
+            'techList' => array_merge(array($techAvail[0], $techAvail[1], $techAvail[2]), $resp),
+            'gitLink' => 'gitlink'
+        ),
+    );
+
+    include ('incl_head.php');
+?>
+    <body class="mainBody">
         <div class="wrapper">
+            <div class="bg">
+                <span>BIKE BUILDER</span>
+            </div>
+            <div class= "gitLinker">
+                <span><a href="https://github.com/amarssadal/Amarjeet.io" target="_blank">View website source on Git</a></span>
+                <ion-icon class="mainGit" name="logo-github"></ion-icon>
+            </div>
             <div class="logoBox">
-                <div class="mainLogo">
-                    <img src="images/logo.png">
-                </div>
+                <img class="logoImg" src="images/logo.png">
             </div>
             <div class="linkBox">
-                <div class="linkBoxContent">
-                    <ul class="link">    
-                        <span class="left bike">
-                            the oustanding
-                        </span>
-                        Bike Builder
-                        <span class="right bike">
-                            web app...kind of
-                        </span>
-                    </ul>
-                    <ul class="link">    
-                        <span class="left bike">
-                            precision
-                        </span>
-                        Contact Tracing
-                        <span class="right bike">
-                        </span>
-                    </ul>
-                    <ul class="link">    
-                        <span class="left bike">
-                            
-                        </span>
-                        Snake Game
-                        <span class="right bike">
-                            classic modern
-                        </span>
-                    </ul>
-                    <ul class="link">    
-                        <span class="left bike">
-                            sharp and on-point
-                        </span>
-                        Corona Timer
-                        <span class="right bike">
-                            to remind us all
-                        </span>
-                    </ul>
-                    <ul class="link">    
-                        <span class="left bike">
-                        </span>
-                        Calculator
-                        <span class="right bike">
-                            to calculate all
-                        </span>
-                    </ul>
-
-                    <ul class="link">    
-                        <span class="left bike">
-                        </span>
-                        To-Do App
-                        <span class="right bike">
-                            keeps you on track
-                        </span>
-                    </ul>
-                    <ul class="link">    
-                        <span class="left bike">
-                            canvas helps 
-                        </span>
-                        Magic Circles
-                        <span class="right bike">
-                            come to life
-                        </span>
-                    </ul>
+                <div class="link">
+                <!-- EMPTY DIV TO SPACE OUT THE FIRST LINKBOX 1000VH DOWN -->
                 </div>
+
+                <?php
+                    foreach($projects as $project) {
+                        $title = $project['title'];
+                        if (strpos($title, ' ') !== false) {
+                            $temp = strpos($title, ' ');
+                            $link = strtolower(substr($title, 0, $temp));
+                        } else {
+                            $temp = $title;
+                            $link = strtolower($temp);
+                        }
+                        
+                        $tech = $project['techList'];
+                        $git = $project['gitLink'];
+                        echo '
+                            <div class="link ' . $link . '">
+                                <div class="linkSub">
+                                    <div class="linkTitle">' . strtoupper($title) . '</div>
+                                        <div class="linkImage">
+                                            <a href="sub.php?page='. $link . '">
+                                                <img class="image" src="images/thumbnails/'. $link . '1.jpg" alt="'. $title . ' Image 1" />
+                                            </a>
+                                            <a href="sub.php?page='. $link . '">
+                                                <img class="image" src="images/thumbnails/'. $link . '2.jpg" alt="' . $title . ' Image 2" />
+                                            </a>
+                                        </div>
+                                        <div class="linkTech">
+                                            ';
+                                            foreach ($tech as $val) {
+                                                echo '<ion-icon class="ionicons" name="' . $val . '"></ion-icon>';
+                                            };
+                                        echo '</div>
+                                        <div class="linkIcons">
+                                            <a href="' . $git . '" target="_blank">
+                                                <ion-icon class="ionicons git" name="logo-github"></ion-icon>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            ';
+                    }
+                ?>
             </div>
-        </div>
+        <script src="main.js"></script>
     </body>
 </html>
--- sub.php
+++ sub.php
@@ -1,44 +1,74 @@
-<!DOCTYPE html>
-<html lang="en">
-    <?php $curr_page = $_GET['page']; ?>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="main.css">
-        <link href="https://fonts.googleapis.com/css2?family=Caveat&family=Nothing+You+Could+Do&family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
-        <title>amarjeet.io - a space for his fooling arounds</title>
+<?php
+    $current = $_GET['page'];
+    $resp = array('tablet-landscape-outline', 'arrow-forward-outline', 'phone-portrait-outline');
+    $techAvail = array('logo-javascript', 'logo-html5', 'logo-css3');
+    $tech = array();
+    switch ($current) {
+        case 'bike':
+            $title = 'Bike Builder';
+            array_push($tech, $techAvail[0], $techAvail[1], $techAvail[2]);
+            $ionics = array_merge($tech, $resp);
+        break;
+        case 'contact':
+            $title = 'Contact Tracing';
+            array_push($tech, $techAvail[0], $techAvail[1], $techAvail[2]);
+            $ionics = array_merge($tech, $resp);
+        break;
+        case 'snake':
+            $title = 'Snake Game';
+            array_push($tech, $techAvail[0], $techAvail[1], $techAvail[2]);
+            $ionics = array_merge($tech, $resp);
+        break;
+        case 'corona':
+            $title = 'Corona Timer';
+            array_push($tech, $techAvail[0], $techAvail[1], $techAvail[2]);
+            $ionics = array_merge($tech, $resp);
+        break;
+        case 'calc':
+            $title = 'Calculator';
+            array_push($tech, $techAvail[0], $techAvail[1], $techAvail[2]);
+            $ionics = array_merge($tech, $resp);
+        break;
+        case 'todo':
+            $title = 'To-Do App';
+            array_push($tech, $techAvail[0], $techAvail[1], $techAvail[2]);
+            $ionics = array_merge($tech, $resp);
+        break;
+        case 'Circles':
+            $title = 'Canvas Circles';
+            array_push($tech, $techAvail[0], $techAvail[1], $techAvail[2]);
+            $ionics = array_merge($tech, $resp);
+        break;
+    }
+    include ('incl_head.php');
+?>
     </head>
-    <body>
-        <div class="subwrapper">
-            <div class="logoBox">
-                    <div class=logoBoxPositioner>
-                        <img class="logoImg" src="images/logo.png">
+    <body class="bodySub">
+        <div class="wrapper">
+            <div class="topBar">
+                <div class="logoBoxSub">
+                    AMARJEET.IO
+                </div>
+                <div class="infoBoxSub">
+                    <div class="pageTitleSub">
+                        Bike Builder
                     </div>
-            </div>
-            <div class="linkBox">
-                <div class="link">
-                    <a href="sub.php?page=bike">Bike Builder</a>
-                </div>
-                <div class="link">
-                    Contact Tracing
-                </div>
-                <div class="link">
-                    Snake Game
-                </div>
-                <div class="link">
-                    Corona Timer
-                </div>
-                <div class="link">
-                    Calculator
-                </div>
-
-                <div class="link">
-                    To-Do App
-                </div>
-                <div class="link">
-                    Magic Circles
+                    <div class="linkTechSub">
+                        <?php
+                            foreach ($ionics as $val) {
+                                echo '<ion-icon class="ionicons" name="' . $val . '"></ion-icon>';
+                            }
+                        ?>
+                    </div>
+                    <div class="linkIconsSub">
+                        <a href="" target="_blank">
+                            <ion-icon class="ionicons git sub" name="logo-github"></ion-icon>
+                        </a>
+                    </div>
                 </div>
             </div>
+            <?php include ('sub/' . $current . '/index.html') ?>
         </div>
+        <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
     </body>
 </html>
